991f249f4003e264461db166a8ee80b4804c212f
dashayu787 <dashayu8818@outlook.com>
2025-08-14 09:56:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,11 @@
-
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  parent: 'game-container',
-  scene: {
-    preload,
-    create,
-    update
-  }
-};
-
-let timerText;
-let scoreText;
-let timeLeft = 60;
-let score = 0;
-let differencePoints = [
-  { x: 120, y: 150 },
-  { x: 300, y: 200 },
-  { x: 500, y: 250 },
-  { x: 220, y: 380 },
-  { x: 450, y: 120 },
-  { x: 650, y: 200 },
-  { x: 600, y: 400 },
-  { x: 350, y: 320 }
-];
-let foundPoints = [];
-
-const game = new Phaser.Game(config);
-
 function preload() {
-  this.load.image('image', 'https://i.imgur.com/2gSOiNm.jpg'); // 示例图片
+  this.load.image('leftImage', 'https://cdn.jsdelivr.net/gh/dashayu787/my-image-cdn/img/level1_a.png');
+  this.load.image('rightImage', 'https://cdn.jsdelivr.net/gh/dashayu787/my-image-cdn/img/level1_b.png');
 }
 
 function create() {
-  this.add.image(200, 300, 'image').setDisplaySize(400, 600);
-  this.add.image(600, 300, 'image').setDisplaySize(400, 600); // 假设左右图一样
+  this.add.image(200, 300, 'leftImage').setDisplaySize(400, 600);
+  this.add.image(600, 300, 'rightImage').setDisplaySize(400, 600);
 
   timerText = this.add.text(10, 10, '剩余时间: 60', { fontSize: '20px', fill: '#000' });
   scoreText = this.add.text(650, 10, '得分: 0', { fontSize: '20px', fill: '#000' });
@@ -70,5 +40,3 @@ function create() {
     loop: true
   });
 }
-
-function update() {}
